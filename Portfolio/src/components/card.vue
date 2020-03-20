9992be0b3fd89956b866515f1d5cb720328d9b19
Eurico Correia <sabrosa1990@gmail.com>
2020-03-20 23:51:12 +0000

    svgs are now working on :hover and on selected

---
+++ Portfolio/src/components/card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <g-image class="cardImage" src />
+    <div class="cardContent">
+      <!-- Project type: Training, Professional -->
+      <p>Project type:</p>
+      <h3>title of the prject</h3>
+      <p>description of the project</p>
+    </div>
+
+    <div class="cardStack"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
