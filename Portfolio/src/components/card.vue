4a73cb46f3e86f9c7c1d98702efcc60b46dd89ef
Eurico Correia <sabrosa1990@gmail.com>
2020-03-21 02:06:53 +0000

    portfolio's cards in place, need to finished them

--- Portfolio/src/components/card.vue
+++ Portfolio/src/components/card.vue
@@ -1,16 +1,88 @@
 <template>
   <div class="card">
-    <g-image class="cardImage" src />
+    <g-image class="cardImage" src="~/assets/images/starter_page2_rasterized.png" />
     <div class="cardContent">
+      <h3>title of the prject</h3>
       <!-- Project type: Training, Professional -->
       <p>Project type:</p>
-      <h3>title of the prject</h3>
-      <p>description of the project</p>
     </div>
 
-    <div class="cardStack"></div>
+    <div class="cardStack">
+      <p>teste</p>
+    </div>
+
+    <div class="cardGithub">
+      <githubCard class="giticon" />
+    </div>
   </div>
 </template>
 
+<script>
+import githubCard from "~/assets/icons/githubCard.svg";
+
+export default {
+  components: {
+    githubCard
+  }
+};
+</script>
+
 <style lang="scss" scoped>
+.card {
+  width: 250px;
+  height: 200px;
+  display: flex;
+  flex-direction: column;
+  font-family: sans-serif;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.9);
+  white-space: nowrap;
+  border-radius: 5px;
+  margin-bottom: 30px;
+}
+.cardImage {
+  width: 50%;
+  height: 60%;
+}
+
+.cardContent {
+  height: 30%;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
+.cardStack {
+  height: 12%;
+  background: rgb(235, 235, 235);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 0 0 5px 5px;
+}
+
+.cardGithub {
+  align-items: center;
+  background: black;
+  border-radius: 50%;
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  position: absolute;
+  transform: translate(200px, 100px);
+  transition: 0.1s ease-in;
+  width: 40px;
+  .giticon {
+    color: #66fcf1;
+  }
+  &:hover {
+    background: #66fcf1;
+    .giticon {
+      color: black;
+    }
+  }
+}
 </style>
--- Portfolio/src/pages/Portfolio.vue
+++ Portfolio/src/pages/Portfolio.vue
@@ -1,16 +1,37 @@
 <template>
   <Layout>
     <h1 class="mainTitle">Portfolio</h1>
+    <div class="allCards">
+      <card />
+      <card />
+      <card />
+      <card />
+      <card />
+      <card />
+    </div>
   </Layout>
 </template>
 
 <script>
+import card from "@/components/card";
+
 export default {
   metaInfo: {
     title: "Portfolio"
+  },
+  components: {
+    card
   }
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.allCards {
+  width: 75%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+
+  flex-wrap: wrap;
+}
 </style>
